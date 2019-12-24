8bda7e88408c9547ad27d48895e39940d9ad06c4
Ørjan Knutli <orjan.knutli@gmail.com>
2019-12-24 16:26:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ function App() {
         ></div>
         <div className="giftDetails">
           <p>
-            I år får du et gavekort på å reise til Oslo når du vil. No question
-            asked.
+          Kjære Ørjan. Jeg vet hvor glad i vin du er så i år får du en elektrisk vi åpner av høyeste kvalitet. God jul fra pappa. 
           </p>
         </div>
       </div>
