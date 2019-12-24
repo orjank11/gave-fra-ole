15c9e4dee90acd370646ce3d79f419d6b04fcab4
Ørjan Knutli <orjan.knutli@gmail.com>
2019-12-24 17:34:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         ></div>
         <div className="giftDetails">
           <p>
-          Kjære Ørjan. Jeg vet hvor glad i vin du er så i år får du en elektrisk vi åpner av høyeste kvalitet. God jul fra pappa. 
+          Kjære Elin. Vi ønsker deg en riktig god jul og inviterer deg herved på en helgetur til Oslo. Klem fra onkel Ole, tante Jana, onkel Even og onkel Ørjan  
           </p>
         </div>
       </div>
